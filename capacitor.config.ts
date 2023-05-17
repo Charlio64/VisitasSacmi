bdae3741f3ccd99f371fbf33b876de59c09c9be2
Carlos Sanchez <carlos.sanchez@sacmigroup.com>
2023-05-17 13:34:41 +0200

    cambio de nombre

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'Actividades',
+  appName: 'Visitas',
   webDir: 'www',
   bundledWebRuntime: false
 };
